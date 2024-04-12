9ac4b6831655a4ad8d7c1937ed4097817d7b65c9
Aiko Vandeputte <aiko@aikovdp.me>
2024-04-12 18:09:26 +0200

    Make emote name optional

--- src/main/java/com/rokucraft/rokuemotes/data/Emote.java
+++ src/main/java/com/rokucraft/rokuemotes/data/Emote.java
@@ -2,17 +2,26 @@ package com.rokucraft.rokuemotes.data;
 
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.configurate.objectmapping.meta.NodeKey;
 
+import static net.kyori.adventure.text.Component.text;
+
+@NullMarked
 public record Emote(
         @NodeKey
         String key,
-        Component name,
+        @Nullable Component name,
         String selfTemplate,
         String otherTemplate,
         Sound sound
 ) {
         public String permission() {
                 return "emote." + key;
+        }
+
+        public Component name() {
+                return name != null ? name : text(key);
         }
 }
